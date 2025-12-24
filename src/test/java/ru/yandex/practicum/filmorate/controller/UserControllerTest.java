08cc67d85a6c9d650bfe735eec769aa9b593f6ed
AnttreePR <valeragazaryan1@yandex.ru>
2025-12-24 12:48:21 +0300

    исправил название классов

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class UserControllerUnitTest {
+class UserControllerTest {
 
     private UserController userController;
 
